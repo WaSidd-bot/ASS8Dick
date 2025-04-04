81a4bef54d37539ae381bcea726d7970c0b04fa0
Wasay Siddiqui <wasaysiddiqui379@gmail.com>
2025-04-04 10:26:54 -0400

    Starter code with IO reader and writer imports

---
+++ src/Dictionary.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.FileNotFoundException;
+
+public class Dictionary {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Hello operator, please enter any sentence in the English language:");
+
+    }
+}
